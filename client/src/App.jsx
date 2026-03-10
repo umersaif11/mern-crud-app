b4c406c0f0f21934ee4bab2f3d5c898ebcf61085
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-03-10 19:38:53 +0500

    Create routes for the app using react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={}></Route>
+          <Route path='/create' element={}></Route>
+          <Route path='/update' element={}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
