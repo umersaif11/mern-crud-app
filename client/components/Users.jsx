362ab6578324f6c13233b49d78a61d3d9aa931dd
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-03-10 21:00:11 +0500

    Make the table for the ui

--- client/components/Users.jsx
+++ client/components/Users.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Users() {
+    const [users, SetUsers] = useState([{
+        Name: "yousaf", Email: "ysf@gmail.com", Age: 20
+    }])
   return (
-    <div>Users</div>
+    <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
+        <div className='w-50 bg-white rounded p-3'>
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Age</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
   )
 }
 
