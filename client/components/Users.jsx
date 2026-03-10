700afe70d2c4303ee203778867189bb9aec94c17
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-03-10 21:17:51 +0500

    Add the button above table using Link

--- client/components/Users.jsx
+++ client/components/Users.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function Users() {
     const [users, SetUsers] = useState([{
@@ -7,6 +8,7 @@ function Users() {
   return (
     <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
         <div className='w-50 bg-white rounded p-3'>
+            <Link to='/create' className='btn btn-success'>Add +</Link>
             <table className='table'>
                 <thead>
                     <tr>
