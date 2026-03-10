4faa4184560ff3535a022e1cd02f2ce90d8bcdaa
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-03-10 20:03:41 +0500

    Import components in the app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
+
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import Users from '../components/Users'
+import CreateUser from '../components/CreateUser'
+import UpdateUser from '../components/UpdateUser'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={}></Route>
-          <Route path='/create' element={}></Route>
-          <Route path='/update' element={}></Route>
+          <Route path='/' element={<Users/>}></Route>
+          <Route path='/create' element={<CreateUser/>}></Route>
+          <Route path='/update' element={<UpdateUser/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
