0acdfb04109be26809616a483bc8d345d48898c7
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-03-14 21:42:12 +0500

    Connect server to mongo db

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect('mongodb://localhost:27017')
+mongoose.connect('mongodb://127.0.0.1:27017')
 app.listen(3001, () => {
     console.log('Server is Running')
 })
