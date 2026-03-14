2b06c4b4566c99094f0923b1a042c0e5862eaaef
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-03-14 22:12:18 +0500

    import the usermodel in index

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const UserModel = require('./models/Users')
 
 const app = express()
 app.use(cors())
