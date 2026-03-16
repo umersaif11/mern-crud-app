c8a4396643f2e1a7ddfdd7b0194e4d9814ab36ea
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-03-16 19:06:17 +0500

    In submitting form axios should send the object to backend and get the response sent

--- client/components/CreateUser.jsx
+++ client/components/CreateUser.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import { useState } from 'react'
+import axios from 'axios'
 
 function CreateUser() {
   const [name, setName] = useState()
   const [email, setEmail] = useState()
   const [age, setAge] = useState()
+  const Submit = (e) => {
+    e.preventDefault()
+    axios.post('http://localhost:3001/createUser', {name, email, age})
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+  }
   return (
     <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
       <div className='w-50 bg-white rounded p-3'>
-        <form>
+        <form onSubmit={Submit}>
           <h2>Add User</h2>
           <div className='mb-2'>
             <label htmlFor="">Name</label>
