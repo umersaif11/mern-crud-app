c9ea7ef864c18aa03bd6c8a9559f661a64f75867
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-03-16 17:17:03 +0500

    Set usestate hook for name, email and age and set the onchange event on each div

--- client/components/CreateUser.jsx
+++ client/components/CreateUser.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useState } from 'react'
 
 function CreateUser() {
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+  const [age, setAge] = useState()
   return (
     <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
       <div className='w-50 bg-white rounded p-3'>
@@ -8,15 +12,18 @@ function CreateUser() {
           <h2>Add User</h2>
           <div className='mb-2'>
             <label htmlFor="">Name</label>
-            <input type="text" placeholder='Enter Name' className='form-control'/>
+            <input type="text" placeholder='Enter Name' className='form-control'
+            onChange={(e) => setName(e.target.value)}/>
           </div>
           <div className='mb-2'>
             <label htmlFor="">Email</label>
-            <input type="email" placeholder='Enter Email' className='form-control'/>
+            <input type="email" placeholder='Enter Email' className='form-control'
+            onChange={(e) => setEmail(e.target.value)}/>
           </div>
           <div className='mb-2'>
             <label htmlFor="">Age</label>
-            <input type="text" placeholder='Enter Age' className='form-control'/>
+            <input type="text" placeholder='Enter Age' className='form-control'
+            onChange={(e) => setAge(e.target.value)}/>
           </div>
           <button className='btn btn-success'>Submit</button>
         </form>
