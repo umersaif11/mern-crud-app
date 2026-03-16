bab18db92751b3e08d9d6838e5c74cbc547a5215
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-03-16 17:09:51 +0500

    Create a post request , get data from react in req, send and save it to DB using module and then send the react back response in json form

--- server/index.js
+++ server/index.js
@@ -7,6 +7,12 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.post('/createUser', (req, res) => {
+    UserModel.create(req.body)
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
 mongoose.connect('mongodb://127.0.0.1:27017')
 app.listen(3001, () => {
     console.log('Server is Running')
