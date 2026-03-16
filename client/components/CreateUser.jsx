34e82b04483c15dda122239bff57e5f338701d6a
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-03-16 20:31:16 +0500

    Once react gets the conformation that data is stored in mongodb then webpage should navigate to homepage so user could know that form worked

--- client/components/CreateUser.jsx
+++ client/components/CreateUser.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { useState } from 'react'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 function CreateUser() {
   const [name, setName] = useState()
   const [email, setEmail] = useState()
   const [age, setAge] = useState()
+  const navigate = useNavigate()
+
   const Submit = (e) => {
     e.preventDefault()
     axios.post('http://localhost:3001/createUser', {name, email, age})
-    .then(res => console.log(res))
+    .then(res => {
+      console.log(res)
+      navigate('/')
+    })
     .catch(err => console.log(err))
   }
   return (
