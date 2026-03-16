213f719d1ab253afeae8455ea46d3c0de7d614e9
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-03-16 20:31:34 +0500

    put /crud at end of mongodb connection path

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.post('/createUser', (req, res) => {
     .catch(err => res.json(err))
 })
 
-mongoose.connect('mongodb://127.0.0.1:27017')
+mongoose.connect('mongodb://127.0.0.1:27017/crud')
 app.listen(3001, () => {
     console.log('Server is Running')
 })
