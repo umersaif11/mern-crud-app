0720fb7987d3f61acf21ab596277da9eb9d0fd0d
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-03-17 21:26:47 +0500

    On submitting form, axios should send the new name,age etc to the backend so that database update them

--- client/components/UpdateUser.jsx
+++ client/components/UpdateUser.jsx
@@ -18,10 +18,20 @@ function UpdateUser() {
       })
       .catch(err => console.log(err))
   },[])
+  
+  const Update = (e) => {
+    e.preventDefault()
+    axios.put('http://localhost:3001/updateUser/'+id, {name, email, age})
+    .then(res => {
+      console.log(res)
+      navigate('/')
+    })
+    .catch(err => console.log(err))
+  }
   return (
     <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
       <div className='w-50 bg-white rounded p-3'>
-        <form>
+        <form onSubmit={Update}>
           <h2>Update User</h2>
           <div className='mb-2'>
             <label htmlFor="">Name</label>
