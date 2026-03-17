d800ef61f0952678a92f18ea0de79ada4cf82ab8
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-03-17 16:19:49 +0500

    Set the get route for home adress and pull all users from database and send them to axios waiting in the front end

--- server/index.js
+++ server/index.js
@@ -7,13 +7,19 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+mongoose.connect('mongodb://127.0.0.1:27017/crud')
+
+app.get('/', (req, res) => {
+    UserModel.find({})
+    .then(users => res.send(users))
+    .catch(err => res.json(err))
+})
 app.post('/createUser', (req, res) => {
     UserModel.create(req.body)
     .then(users => res.json(users))
     .catch(err => res.json(err))
 })
 
-mongoose.connect('mongodb://127.0.0.1:27017/crud')
 app.listen(3001, () => {
     console.log('Server is Running')
 })
