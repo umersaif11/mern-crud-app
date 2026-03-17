fcdc3157369cec08af548801abcd568f9b62c6eb
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-03-17 16:53:38 +0500

    Pass the user id in the update route

--- client/components/Users.jsx
+++ client/components/Users.jsx
@@ -31,7 +31,7 @@ function Users() {
                                 <td>{user.email}</td>
                                 <td>{user.age}</td>
                                 <td>
-                                    <Link to='/update' className='btn btn-success'>Update</Link>
+                                    <Link to={`/update/${user._id}`} className='btn btn-success'>Update</Link>
                                     <button className='btn btn-danger'>Delete</button>
                                 </td>
                             </tr>
