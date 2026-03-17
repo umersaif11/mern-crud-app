00ffd25836919423d529d6056efe1cb8e5cc2176
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-03-17 17:05:57 +0500

    useParams is used to get the parametres in the url

--- client/components/UpdateUser.jsx
+++ client/components/UpdateUser.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
 function UpdateUser() {
+  const {id} = useParams()
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+  const [age, setAge] = useState()
+  const navigate = useNavigate()
   return (
     <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
       <div className='w-50 bg-white rounded p-3'>
